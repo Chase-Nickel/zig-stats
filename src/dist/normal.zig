2108987c427782f3b5ee6d9477960069daa28787
Chase Nickel <nickel5@outlook.com>
2024-06-20 18:01:42 -0500

    More functions for normal distribution

--- src/dist/normal.zig
+++ src/dist/normal.zig
@@ -26,5 +26,33 @@ pub fn fromSample(sample: []const f64) Self {
     };
 }
 
+pub fn median(self: Self) f64 {
+    return self.mean;
+}
+
+pub fn mode(self: Self) f64 {
+    return self.mean;
+}
+
+pub fn variance(self: Self) f64 {
+    return self.stdev * self.stdev;
+}
+
+pub fn mad(self: Self) f64 {
+    return self.stdev * @sqrt(2.0 / std.math.pi);
+}
+
+pub fn pdf(self: Self, x: f64) f64 {
+    _ = self; // autofix
+    _ = x; // autofix
+    @compileError("Not implemented");
+}
+
+pub fn cdf(self: Self, x: f64) f64 {
+    _ = self; // autofix
+    _ = x; // autofix
+    @compileError("Not implemented");
+}
+
 const stats = @import("../stats.zig");
 const std = @import("std");
