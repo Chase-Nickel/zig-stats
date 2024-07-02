6e653e04f0d63b09395b99709e19e09414ffd8dc
Chase Nickel <nickel5@outlook.com>
2024-07-01 23:08:40 -0500

    Add check step to build.zig. Uses -fno-emit-bin for fast comp

--- build.zig
+++ build.zig
@@ -11,4 +11,13 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/stats.zig"),
     });
     b.installArtifact(lib);
+
+    const lib_check = b.addStaticLibrary(.{
+        .target = target,
+        .optimize = optimize,
+        .name = "zig-stats",
+        .root_source_file = b.path("src/stats.zig"),
+    });
+    const check = b.step("check", "Check if the project builds");
+    check.dependOn(&lib_check.step);
 }
